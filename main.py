304dd5af8f73e4c3323fd62c97d5c3dd5003d0f2
Yiming Zhang <yimingz@umich.edu>
2020-07-09 02:28:08 -0400

    remove d_lr

--- main.py
+++ main.py
@@ -63,7 +63,6 @@ def main():
     )
     parser.add_argument("--ner-lr", default=2e-5, type=float)
     parser.add_argument("--g-lr", default=1e-6, type=float)
-    parser.add_argument("--d-lr", default=1e-4, type=float)
     parser.add_argument("--weight_decay", default=0.0, type=float, help="Weight decay if we apply some.")
     parser.add_argument("--adam_epsilon", default=1e-8, type=float, help="Epsilon for Adam optimizer.")
     parser.add_argument("--max_grad_norm", default=1.0, type=float, help="Max gradient norm.")
