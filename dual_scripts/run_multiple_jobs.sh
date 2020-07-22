b48d561a5b8f8a921610c598707cd57a478a33bf
Yiming Zhang <yimingz@umich.edu>
2020-07-22 03:19:04 -0400

    what

--- dual_scripts/run_multiple_jobs.sh
+++ dual_scripts/run_multiple_jobs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
-for FILE in dual_std0.1_3epochs_skipner.py dual_std0.5_3epochs_skipner.py dual_td0.1_3_skipner_nt.py dual_td0.1_3_skipner.py
+for FILE in dual_td0.1_3_skipner_nt.py dual_td0_3_skipner_nt.py dual_sstd1_3_skipner_nt.py
 do
-for LANG in he bg fa fi
+for LANG in he bg fa fi af ar bn de el es et fr hi hu id it ms nl pt ru ta tl tr vi
 do
 python3 $FILE $LANG > $FILE-en-$LANG.sh
 sbatch $FILE-en-$LANG.sh
