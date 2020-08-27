46e358ac51188f01af9f11e4e155b22cec2512d0
Yiming Zhang <yimingz@umich.edu>
2020-08-27 07:57:05 -0400

    a

--- dual_scripts/run_multiple_jobs.sh
+++ dual_scripts/run_multiple_jobs.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
-for FILE in dual_td0_3_skipner_nt.py dual_sstd1_3_skipner_nt.py
-#for FILE in dual_td0.1_3_skipner_nt.py
+for FILE in dual_std0.1_lowlr.py
 do
 for LANG in he bg fa fi af ar bn de el es et fr hi hu id it ms nl pt ru ta tl tr vi
 do
